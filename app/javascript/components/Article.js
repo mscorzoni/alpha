d8958e6ddb7b12a70132e496e5ad9b1e1e3364a7
Marcos Scorzoni <mlessa@gmail.com>
2018-09-09 14:39:41 +0100

    add timestamp component

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,5 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Timestamp from 'react-timestamp'
+
+
 class Article extends React.Component {
   render () {
     return (
@@ -12,7 +15,7 @@ class Article extends React.Component {
         </div>
         <div className="article-meta-details">
             <small>Created by: {this.props.author}, {this.props.created_at} ago,
-                  last updated: {this.props.updated_at} %> ago</small>  
+                  last updated: {this.props.updated_at} ago</small>  
           </div>
       </React.Fragment>
     );
