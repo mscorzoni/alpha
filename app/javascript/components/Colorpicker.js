7e8ce36c26b9cf9a9dc5fddf0d2511d68d51263d
Marcos Scorzoni <mlessa@gmail.com>
2018-09-10 18:28:10 +0100

    add dynamic color to colorpicker component

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types'
 import { SketchPicker } from 'react-color'
 
 class Colorpicker extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (color) => {
+    document.body.style.background = color.hex;
+  }
+
   render() {
     return  (<React.Fragment>
-              <SketchPicker />
+              <SketchPicker color={this.props.color}
+                            onChange={this.handleChange}/>
             </React.Fragment>)
   }
 }
