eebcb3ffab011069693fdd1c99daef57721136ec
Marcos Scorzoni <mlessa@gmail.com>
2018-09-10 12:16:57 +0100

    add colorpicker

---
+++ app/javascript/components/Colorpicker.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Colorpicker extends React.Component {
+  render() {
+    return  (<React.Fragment>
+              <div>Hello</div>
+            </React.Fragment>)
+  }
+}
+
+Colorpicker.propTypes = {};
+
+export default Colorpicker;
